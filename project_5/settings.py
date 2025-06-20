0817bda519fb35886f35fd16ed1dc4b6b5ce863f
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-21 00:41:50 +0200

    Code: Register custom apps (accounts, orders, portfolio) in INSTALLED_APPS.

---
+++ project_5/settings.py
@@ -0,0 +1,13 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttype',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Egna appar
+    'accounts',
+    'orders',
+    'portfolio',
+]
