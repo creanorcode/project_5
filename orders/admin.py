8f2eea77953180bdd4a498fcecfb1711fc688b88
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-21 00:23:49 +0200

    Code improvement according to Flake8 in admin.py.

--- orders/admin.py
+++ orders/admin.py
@@ -8,20 +8,18 @@ so they can be managed through the Django admin interface.
 """
 
 
-
 @admin.register(DesignType)
 class DesignTypeAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
 
 
-
 @admin.register(DesignOrder)
 class DesignOrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'customer', 'design_type', 'size', 'quote', 'paid', 'created_at',)
+    list_display = ('id', 'customer', 'design_type', 'size', 'quote',
+                    'paid', 'created_at',)
     list_filter = ('paid', 'created_at',)
     search_fields = ('customer_username', 'description',)
-
 
 
 @admin.register(CompletedDesign)
