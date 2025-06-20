40c351afde96c3e32efdcbaf3c473b14045981b7
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-21 00:48:01 +0200

    Fix typo in INSTALLED_APPS: correct 'contenttype' to 'contenttypes'.

--- project_5/settings.py
+++ project_5/settings.py
@@ -1,7 +1,7 @@
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttype',
+    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
