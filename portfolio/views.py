8b627c0ddaba91054a03b6e2f22a419d38fd6e1f
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-23 22:17:37 +0200

    CODE: Added views for home and portfolio pages.

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'portfolio/home.html')
+
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
