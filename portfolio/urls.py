8346473e258150fb785b473e2d730b5808cbb2c3
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-23 23:55:32 +0200

    CODE: Add correct URL patterns for home and portfolio pages.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('portfolio/', views.portfolio, name='portfolio'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('portfolio/', TemplateView.as_view(template_name='portfolio.html'), name='portfolio'),
 ]
