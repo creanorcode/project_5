8f6f3b98ecfe7809d33f32b0ac314c3a4437bd36
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-24 00:38:47 +0200

    Changed accounts/urls.py to LoginView and LogoutView.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
-    path('register/', TemplateView.as_view(template_name='register.html'), name='register'),
-    path('logout/', TemplateView.as_view(template_name='logout.html'), name='logout'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
