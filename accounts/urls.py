9a59b7da990f4d3bebc6c06bf69768560c62c62e
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-24 00:00:43 +0200

    CODE: Add URL patterns for login, register and logout pages.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
+    path('register/', TemplateView.as_view(template_name='register.html'), name='register'),
+    path('logout/', TemplateView.as_view(template_name='logout.html'), name='logout'),
 ]
