e3aabe7a690f2bdfa900486811e5ab39e056485b
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-23 23:57:33 +0200

    CODE: Add correct URL pattern for order (design_order) page.

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('design/', views.design_order, name='design_order'),
+    path('', TemplateView.as_view(template_name='design_order.html'), name='order'),
 ]
