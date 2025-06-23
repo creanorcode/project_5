673a9b8f93925992a8e7cf97a8890bf619475eeb
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-23 22:12:34 +0200

    CODE: Set up project-level URL configuration and included app URLs.

--- project_5/urls.py
+++ project_5/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('portfolio.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('', include('portfolio.urls')), # home and portfolio
+    path('orders/', include('orders.urls')), # design order
+    path('accounts/', include('accounts.urls')), # Login, Logout, register
+    path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'), # contact
 ]
