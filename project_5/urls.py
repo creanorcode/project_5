f677bb359af8d3e27ac04e6471c580d0ffa7cec7
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-24 00:08:25 +0200

    FIX: Changed 'order' to 'design_order' in project_5/urls.py to harmonize with base.html.

--- project_5/urls.py
+++ project_5/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),  # home
     path('portfolio/', TemplateView.as_view(template_name='portfolio.html'), name='portfolio'),  # portfolio
-    path('order/', TemplateView.as_view(template_name='design_order.html'), name='order'),  # design order
+    path('order/', TemplateView.as_view(template_name='design_order.html'), name='design_order'),  # design order
     path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),  # contact
     path('login/', TemplateView.as_view(template_name='login.html'), name='login'),  # login
     path('register/', TemplateView.as_view(template_name='register.html'), name='register'),  # register
