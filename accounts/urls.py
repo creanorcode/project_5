cb7270f44db3aa89f3defa8ec271cdbe1dd1ad54
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-25 01:21:12 +0200

    CODE: Replace Django built-in LoginView with custom login_view i views.py in urls.py.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
