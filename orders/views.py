73f551512a5f0826c7088fbd046b685795c12b82
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-24 12:47:25 +0200

    CODE: Add confirmation message after design order submission.

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.contrib import messages
+from .forms import DesignOrderForm
 
 
 def design_order(request):
-    return render(request, 'design_order.html'),
+    if request.method == 'POST':
+        form = DesignOrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your design order har been submitted. We will get back to you shortly.')
+            return redirect('portfolio')
+    else:
+        form = DesignOrderForm()
+    return render(request, 'design_order.html', {'form': form})
