8f58a734cbacb71b4f051fdecdea5ffadea02839
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-24 22:02:27 +0200

    CODE: Add contact_view to handle form submission and feedback messages on contact page.

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.contrib import messages
+
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            messages.success(request, "Thank you for your message! We´ll be in touch soon.")
+            form = ContactForm()  # Empty the form
+    else:
+        form = ContactForm()
+    return render(render, 'contact.html', {'form': form})
 
 
 def home(request):
