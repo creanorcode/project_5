e6ff4b77cbf1a2f5ee27ef83f509e676961c0328
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-25 17:26:54 +0200

    Remove conflicting login path in main urls.py to enable accounts.login_view.

--- project_5/urls.py
+++ project_5/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('order/', TemplateView.as_view(template_name='design_order.html'), name='design_order'),  # design order
     path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),  # contact
     path('accounts/', include('accounts.urls')),  # djangos layout for login, logout, register
-    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),  # login
+   # path('login/', TemplateView.as_view(template_name='login.html'), name='login'),  # login
     path('register/', TemplateView.as_view(template_name='register.html'), name='register'),  # register
     path('', include('portfolio.urls')),
 ]
