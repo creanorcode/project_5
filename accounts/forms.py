3111f7a5f356b4ba378b58ae62d12a91c8a00190
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-26 13:25:52 +0200

    CODE: Improve UserRegisterForm: unique email validation and clean UI help_texts.

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,9 +2,19 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        help_texts = {
+            'username': None
+        }
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email address is already in use.")
+        return email
