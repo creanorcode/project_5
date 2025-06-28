b633a52adec1fb2b444d57086c2f84775b0869fc
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-28 13:51:50 +0200

    FEAT: Add cart views for detail, add and remove.

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from products.models import Product
+from .models import CartItem
 
-# Create your views here.
+
+def cart_detail(request):
+    cart_items = CartItem.objects.all()
+    total_price = sum(item.product.price * item.quantity for item in cart_items)
+    return render(request, 'cart/cart_detail.html', {
+        'cart_items': cart_items,
+        'total_price': total_price,
+    })
+
+
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    cart_item, created = CartItem.objects.get_or_create(product=product)
+    if not created:
+        cart_item.quantity += 1
+    cart_item.save()
+    return redirect('cart:cart_detail')
