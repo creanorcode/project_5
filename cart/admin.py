413ff891e9d3472c6dbd7ec91187417d3fd899ae
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-28 13:33:36 +0200

    FEAT: Register CartItem model in Django admin.

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CartItem
 
-# Register your models here.
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'quantity', 'created_at')
+    search_fields = ('user_username', 'product_title')
