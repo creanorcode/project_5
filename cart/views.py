ee2b321caa3958befd01181976f65a7a1281e27b
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-28 22:14:15 +0200

    FIX: Updated def add_to_cart in views.py with get in cart(str(product_id)).

--- cart/views.py
+++ cart/views.py
@@ -56,7 +56,7 @@ def add_to_cart(request, product_id):
     else:
         # use session cart
         cart = request.session.get('cart', {})
-        cart[str(product_id)] = cart(str(product_id), 0) + 1
+        cart[str(product_id)] = cart.get(str(product_id), 0) + 1
         request.session['cart'] = cart
 
     return redirect('cart:cart_detail')
