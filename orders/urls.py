70dfe6ac085df3396c3801aef926998621feb187
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-29 12:15:47 +0200

    CODE: Add urls for order list and checkout.

--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='design_order.html'), name='design_order'),
+    path('', views.order_list, name='order_list'),
+    path('checkout/', views.checkout, name='checkout'),
 ]
