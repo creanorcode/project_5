ee39e59e391eb3210446e4ec0fff46ef8d15375f
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-29 22:32:22 +0200

    CODE: Add ContactMessageForm as ModelForm for contact messages.

--- contact/forms.py
+++ contact/forms.py
@@ -1,8 +1,14 @@
 # contact/forms.py
 from django import forms
+from .models import ContactMessage
 
 
-class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, label='Your Name')
-    email = forms.EmailField(label='Your Email')
-    message = forms.CharField(widget=forms.Textarea, label='Your Message')
+class ContactMessageForm(forms.ModelForm):
+    class Meta:
+        model = ContactMessage
+        fields =['name', 'email', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your name'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your email'}),
+            'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Your message', 'rows': 5}),
+        }
