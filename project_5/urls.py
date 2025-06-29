efd8d1345e8f72c5ca3e53e4010f72b1dcdc3fdc
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-29 22:47:51 +0200

    CODE: Include contact app URLs in main project routing.

--- project_5/urls.py
+++ project_5/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),  # home
     path('portfolio/', TemplateView.as_view(template_name='portfolio.html'), name='portfolio'),  # portfolio
     path('order/', TemplateView.as_view(template_name='design_order.html'), name='design_order'),  # design order
-    path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),  # contact
+    # path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),  # contact
     path('accounts/', include('accounts.urls')),  # djangos layout for login, logout, register
     # path('login/', TemplateView.as_view(template_name='login.html'), name='login'), login
     # path('register/', TemplateView.as_view(template_name='register.html'), name='register'), register
@@ -17,6 +17,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
+    path('contact/', include('contact.urls', namespace='contact')),
 ]
 
 if settings.DEBUG:
