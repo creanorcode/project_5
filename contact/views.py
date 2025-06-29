714377437dfea8c398d48ff1a781269152920f33
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-29 22:24:28 +0200

    CODE: Add contact_view to handle contact from submissions and save to database.

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactMessageForm
 
-# Create your views here.
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactMessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Thank you for your message! We will get back to you soon.")
+            return redirect('contact:contact')
+    else:
+        form = ContactMessageForm()
+    
+    return render(request, 'contact/contact.html', {'form': form})
