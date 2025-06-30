edc1f4292e4376ff0fbef0d05329b4a0fb8fbbbb
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-06-30 18:40:17 +0200

    FIX: Fixing a path issue with design_order in project_5/urls.py.

--- project_5/urls.py
+++ project_5/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),  # home
     path('portfolio/', TemplateView.as_view(template_name='portfolio.html'), name='portfolio'),  # portfolio
-    path('order/', TemplateView.as_view(template_name='design_order.html'), name='design_order'),  # design order
+    # path('order/', TemplateView.as_view(template_name='design_order.html'), name='design_order'),  # design order
     # path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),  # contact
     path('accounts/', include('accounts.urls')),  # djangos layout for login, logout, register
     # path('login/', TemplateView.as_view(template_name='login.html'), name='login'), login
