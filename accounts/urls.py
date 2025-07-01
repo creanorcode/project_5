6ed9eb1db72deb8488fd8f1af1bcb165f2c99f57
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-07-01 04:15:52 +0200

    CODE: Add password reset views and URL patterns with templates.

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,19 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='accounts/password_reset.html'
+    ), name='password_reset'),
+
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'
+    ), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='accounts/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
--- project_5/settings.py
+++ project_5/settings.py
@@ -103,6 +103,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+# E-mail configuration
+EMAIL_BACKEND = 'django.core.mail.backend.console.EmailBackend'
+
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
