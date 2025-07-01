92bc367992575286c49c3d772671340e428908b5
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-07-01 05:36:06 +0200

    Debug to false for production and allowed hosts.

--- project_5/settings.py
+++ project_5/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-dev-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['artea.herokuapp.com', 'www.artea.studio', 'localhost']
 
 # Application definition
 INSTALLED_APPS = [
