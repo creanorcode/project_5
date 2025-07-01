e96547979f1a191033b1ca0fab48ebbb0b4ae4ca
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-07-01 07:18:33 +0200

    Updated .env with email configuration.

--- project_5/settings.py
+++ project_5/settings.py
@@ -111,7 +111,13 @@ USE_I18N = True
 USE_TZ = True
 
 # E-mail configuration
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", 'no-reply@artea.studio')
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
