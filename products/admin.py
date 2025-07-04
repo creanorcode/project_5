5072d80b7e05c5465bfc495068ca10f6e7dfa1dd
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-07-04 14:28:43 +0200

    Fix ProductAdmin fields to include image and make created_at readonly.

--- products/admin.py
+++ products/admin.py
@@ -4,6 +4,6 @@ from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price')
-    fields = ('title', 'price', 'description', 'created_at')
+    list_display = ('title', 'price', 'created_at', 'image')
+    fields = ('title', 'price', 'description', 'image', 'created_at')
     readonly_fields = ('created_at')
