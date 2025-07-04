89de644834b0d4b43619a1cbcae5f79cc86b828c
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-07-04 23:36:31 +0200

    Add storage debug file

---
+++ check_storage.py
@@ -0,0 +1,9 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_5.settings")
+django.setup()
+
+from django.conf import settings
+
+print("DEFAULT_FILE_STORAGE is", settings.DEFAULT_FILE_STORAGE)
