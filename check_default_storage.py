f454e706aea0ca5609ee5df27e174cf01827c66c
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-07-05 15:33:55 +0200

    Add debug script for checking default storage

---
+++ check_default_storage.py
@@ -0,0 +1,8 @@
+from django.core.files.storage import default_storage
+from django.conf import settings
+
+print("DEBUG =", settings.DEBUG)
+print("DEFAULT_FILE_STORAGE =", settings.DEFAULT_FILE_STORAGE)
+print("default_storage class =", default_storage.__class__)
+print("default_storage base location =", getattr(default_storage, 'location', 'no .location attribute'))
+print("default_storage bucket name =", getattr(default_storage, 'bucket_name', 'no .bucket_name attribute'))
