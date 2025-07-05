a38a1a9d7f97674e44a5cea28a502cd9ae303578
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-07-05 15:46:45 +0200

    Fix import_string for Django 5

--- project_5/settings.py
+++ project_5/settings.py
@@ -182,8 +182,8 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-from django.core.files.storage import default_storage
-from django.core.files.storage import get_storage_class
+from django.core.files import storage
+from django.utils.module_loading import import_string
 
 if DEFAULT_FILE_STORAGE != 'django.core.files.storage.FileSystemStorage':
-    default_storage._wrapped = get_storage_class(DEFAULT_FILE_STORAGE)()
+    default_storage._wrapped = import_string(DEFAULT_FILE_STORAGE)()
