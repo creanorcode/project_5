81a8c3ba6bce70783c1c4e7a49f241f7d209c1b7
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-07-06 17:24:06 +0200

    Fix custom storage to remove default_acl for bucket owner enforced policy.

--- custom_storages.py
+++ custom_storages.py
@@ -4,9 +4,9 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
-    default_acl = 'public-read'
+    default_acl = None
 
     
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
-    default_acl = 'public-read'
+    default_acl = None
