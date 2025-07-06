bdbf70918da34cceeee15fc7559933ab2b395edd
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-07-06 17:01:39 +0200

    Use STATICFILES_LOCATION and MEDIAFILES_LOCATION for custom S3 storage.

--- custom_storages.py
+++ custom_storages.py
@@ -3,10 +3,10 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = 'static'
+    location = settings.STATICFILES_LOCATION
     default_acl = 'public-read'
 
     
 class MediaStorage(S3Boto3Storage):
-    location = 'media'
+    location = settings.MEDIAFILES_LOCATION
     default_acl = 'public-read'
