465efd5bd03edb73d39572a0c323699fde915173
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-07-07 13:16:57 +0200

    Add explicit bucket_name to MEdiaStorage class for correct S3 upload.

--- project_5/custom_storages.py
+++ project_5/custom_storages.py
@@ -4,4 +4,5 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
     default_acl = None
