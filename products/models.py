15a455f3c402849838f3d65d296d71ecc97a073f
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-07-07 14:22:37 +0200

    FIX: Force Product.image to use s# MediaStorage explicity to solve upload issues.

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from project_5.custom_storages import MediaStorage
 
 from django.core.files.storage import default_storage
 print("DEBUG: products.models Product.image will use", default_storage)
@@ -6,7 +7,7 @@ print("DEBUG: products.models Product.image will use", default_storage)
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='product_images/')
+    image = models.ImageField(upload_to='product_images/', storage=MediaStorage())
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
