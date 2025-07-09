fda42651b57c4e0ccd61b23585c62da75889b826
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-07-09 21:50:25 +0200

    Show user_reply and replied_at in ContactMessage admin panel.

--- contact/admin.py
+++ contact/admin.py
@@ -7,6 +7,14 @@ class ContactMessageAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created_at', 'is_answered')
     list_filter = ('is_answered',)
     search_fields = ('name', 'email' 'message')
-    readonly_fields = ('name', 'email', 'message', 'created_at')
-    fields = ('name', 'email', 'message', 'created_at', 'admin_reply', 'is_answered')
+    readonly_fields = (
+        'name', 'email', 'message', 'created_at',
+        'user_reply', 'replied_at'
+    )
+    fields = (
+        'name', 'email', 'message', 'created_at',
+        'user_reply', 'admin_reply', 'is_answered'
+    )
     ordering = ('-created_at',)
+
+admin.site.register(ContactMessage, ContactMessageAdmin)
