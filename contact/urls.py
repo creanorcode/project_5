8ed809cf58dc024bc9c60e363eaf21dd4e3a0962
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-07-10 14:08:53 +0200

    Update URL pattern for thread_detail_view.

--- contact/urls.py
+++ contact/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import new_message_view, contact_thread_view, thread_list_view
+from .views import new_message_view, contact_thread_view, thread_list_view, thread_detail_view
 
 
 app_name = 'contact'
@@ -9,8 +9,8 @@ app_name = 'contact'
 urlpatterns = [
     path('', views.contact_view, name='contact'),
     path('my-messages/', views.user_messages_view, name='user_messages'),
-    path('thread/<int:thread_id>/', views.thread_detail_view, name='thread_detail'),
     path('messages/<int:message_id>/', views.contact_detail_view, name='message_detail'),
+    path('thread/<int:thread_id>/', thread_detail_view, name='thread_detail'),
     path('messages/new/', new_message_view, name='new_message'),
     path('thread/<int:message_id>/', contact_thread_view, name='contact_thread'),
     path('messages/new/', views.create_thread_view, name='create_thread'),
