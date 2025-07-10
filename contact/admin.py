0283b324aab91f769fce8697e7099eebf88b6ec9
creanorcode <p03k9o0w@students.codeinstitute.net>
2025-07-10 21:14:20 +0200

    Add admin action to create MessageThread from ContactMessage.

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,8 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import ContactMessage, MessageThread, Message, ThreadMessage
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 @admin.register(MessageThread)
@@ -15,6 +18,7 @@ class MessageAdmin(admin.ModelAdmin):
 @admin.register(ContactMessage)
 class ContactMessageAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created_at', 'is_answered')
+    actions = ['create_thread_from_message']
     list_filter = ('is_answered',)
     search_fields = ('name', 'email' 'message')
     readonly_fields = (
@@ -26,6 +30,25 @@ class ContactMessageAdmin(admin.ModelAdmin):
         'user_reply', 'admin_reply', 'is_answered'
     )
     ordering = ('-created_at',)
+
+    @admin.action(description='Create conversation thread from this message')
+    def create_thread_from_message(self, request, queryset):
+        for contact in queryset:
+            try:
+                user = User.objects.get(email=contact.email)
+            except User.DoesNotExist:
+                self.message_user(request, f"No user with email {contact.email} found.", level=messages.WARNING)
+                continue
+
+            thread =MessageThread.objects.create(user=user, subject=f"Re: {contact.name}´s message")
+            ThreadMessage.objects.create(
+                thread=thread,
+                sender=user,
+                body=contact.message
+            )
+            contact.is_answered = True
+            contact.save()
+            self.message_user(request, f"Thread created for {contact.name}", level=messages.SUCCESS)
 
 
 admin.site.register(ThreadMessage)
